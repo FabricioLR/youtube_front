7d04c0f43f6dce78b27cd6848f997905524fed93
FabricioLR <dimamitoyt@gmail.com>
2023-05-27 10:13:54 -0300

    update to new api url

--- src/context/api.tsx
+++ src/context/api.tsx
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-//"https://youtubebackcopy.onrender.com"
+//"https://youtubeback-lm98.onrender.com/"
 //"http://localhost:4000/"
 
 const api = axios.create({
-    baseURL: "https://youtubebackcopy.onrender.com"
+    baseURL: "http://localhost:4000/"
 })
 
 export default api
